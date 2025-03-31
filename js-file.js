6ea0d4cc27c0809ffe93748622882eb09afa531b
Akarsh <akarshsp456@gmail.com>
2025-03-31 18:53:32 +0530

    Add gradual opacity improvement feature

--- js-file.js
+++ js-file.js
@@ -4,7 +4,7 @@ function getRandomRGBColor(){
     let r= Math.floor(Math.random() * 256);
     let g= Math.floor(Math.random() * 256);
     let b= Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`;
+    return {r,g,b};
 
 }
 
@@ -21,12 +21,14 @@ function makeGrid(num = 16) {
         square.style.height = size + "px";
         square.style.border = "solid black 1px";
         square.style.boxSizing = "border-box";
-        let hasChanged = false;
+        let color = getRandomRGBColor();
+        let opacity = 0;
 
         square.addEventListener("mouseover", () => {
-            if(!hasChanged){
-            square.style.backgroundColor = getRandomRGBColor();
-            hasChanged = true;
+            if(opacity<1){
+                
+                opacity+= 0.1;
+                square.style.backgroundColor = `rgba(${color.r},${color.g},${color.b},${opacity})`;
             }
         });
 
@@ -39,8 +41,8 @@ restart.addEventListener("click", () => {
     let inpNum = prompt("How many grids do you need?");
     inpNum = parseInt(inpNum);
 
-    if (!inpNum || inpNum <= 0) {
-        alert("Please enter a valid number greater than 0.");
+    if (!inpNum || inpNum <= 0 || inpNum>=100) {
+        alert("Please enter a valid number greater than 0 and less than 100");
         return;
     }
 
